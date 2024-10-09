3f165b60112631dc6aea8966c698b34a6736eb90
yoigarasumomiji <sateraito3696@gmail.com>
2024-10-09 13:24:06 +0900

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.nekosuki.multieditor {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.nekosuki.multieditor to javafx.fxml;
+    exports com.nekosuki.multieditor;
+}
